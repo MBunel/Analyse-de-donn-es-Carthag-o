eea84003ea68e55b759239e26b114c8dc0fc4047
Mattia Bunel <mattia.bunel@ehess.fr>
2024-10-14 12:53:55 +0200

    update nix

--- default.nix
+++ default.nix
@@ -1,10 +1,12 @@
-{ pkgs ? import <nixpkgs> { }, pythonPackages ? pkgs.python3Packages }:
+{ pkgs ? import <nixpkgs> { }, pythonPackages ? pkgs.python3Packages, rPackages ?  kgs.rPackages}:
 
 pkgs.mkShell {
   buildInputs = [
      pythonPackages.numpy
      pythonPackages.scipy
      pythonPackages.jupyterlab
+     rPackages.irkernel
+     rPackages.ggplot2
   ];
 
 }
