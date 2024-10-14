bd0007556c708a85176b06dbb76fc8ba1bfbcf2f
Mattia Bunel <mattia.bunel@ehess.fr>
2024-10-14 12:57:27 +0200

    Update

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> { }, pythonPackages ? pkgs.python3Packages, rPackages ?  kgs.rPackages}:
+{ pkgs ? import <nixpkgs> { }, pythonPackages ? pkgs.python3Packages, rPackages ?  pkgs.rPackages}:
 
 pkgs.mkShell {
   buildInputs = [
